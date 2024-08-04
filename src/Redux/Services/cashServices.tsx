5ce13678c21f6c4a90415c05ea20f5bdbc1a1b74
zxccursed <asobolev925@gmail.com>
2024-08-04 19:24:06 +0400

    5

--- src/Redux/Services/cashServices.tsx
+++ src/Redux/Services/cashServices.tsx
@@ -1,19 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import useLocalStorege from "../../CustomHooks/useLocalStorege";
+
+const token = useLocalStorege('token', null)
 
 export const cashServices = createApi({
     reducerPath: 'cashServices',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api/cash/' }),
     endpoints: (build: any) => ({
-        GETAllUsers: build.query({
+        GETAllcash: build.query({
             query: () => `search/cash/`
         }),
-        POSTUser: build.mutation({
+        ADDcash: build.mutation({
             query: (body_: any) => ({
                 url: 'reg/cash/',
                 method: 'POST',
                 headers: {
                     'Content-type': 'application/json',
-                    'Authorization': `JWT ${() => { const token: any = localStorage.getItem('key') ? JSON.parse(localStorage.getItem('key')) : 'zxc'; return token }}`
+                    'Authorization': `JWT ${token}`
                 },
                 body: JSON.stringify(body_)
             })
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,34 +1,12 @@
-import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../CustomHooks/ReduxHooks";
-import { fetchUsers } from "../Redux/Reducers/ActionCreators";
-import { cashServices } from "../Redux/Services/cashServices";
-
 interface props_ {
 
 }
 
 const Main = ({ }: props_) => {
-    const zxc = useAppSelector(state => state.users.count)
-    const dispatch = useAppDispatch()
-
-    useEffect(() => {
-        fetchUsers(dispatch)
-    }, [])
-    const [cre, dafa] = cashServices.usePOSTUserMutation()
-
-    const z = async () => {
-        await cre({ author: 1, price: 4123, content: 'wqsad', direction: 1 })
-    }
-
-    useEffect(() => {
-        z()
-    }, [])
-
-    console.log(dafa)
 
     return (
         <>
-            <div style={{ margin: '20px' }}>{zxc}</div>
+            <div style={{ margin: '20px' }}></div>
         </>
     );
 }
